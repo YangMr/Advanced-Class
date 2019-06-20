53bc5292b9cf4825119e2bfeb9890a36c3d05a9f
YangMr <759168442@qq.com>
2019-06-20 17:44:55 +0800

    add

---
+++ 第七周/4Day/module.js
@@ -0,0 +1,26 @@
+export default {
+	//获取数据库的数据
+	get : function(){
+		
+	},
+	//设置数据库的数据
+	set : function(){
+		
+	},
+	//修改数据库的数据
+	change : function(){
+		
+	},
+	//删除数据库的数据
+	del : function(){
+		
+	},
+	//解析字符串方法
+	params : function(){
+		
+	},
+	//随机数方法
+	ranDom : function(){
+		
+	}
+}
